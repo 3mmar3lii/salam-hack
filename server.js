486d3f7b74f861625b1026203d66e26490dc832a
3mmar 3li <ammaralaa572@gmail.com>
2026-04-27 10:35:16 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const connectDB = require('./db/db');
+
+
+// Middlewares
+const { errorHandler } = require('./middlewares/error.middleware');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Security & Parsing
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Logging
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+// Database Connection
+connectDB();
+
+// API Routes
+//app.use('/api/v1', v1Routes);
+
+
+
+// Error Handling Middleware
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
+});
